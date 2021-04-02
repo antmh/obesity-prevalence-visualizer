bd36df592b5d28d1074f2369a81f50fea6a529de
Antonio Mihăeș <antnm@protonmail.com>
2021-04-02 18:37:01 +0300

    Update design and refactor HTML & CSS

--- views/components/footer.php
+++ views/components/footer.php
@@ -1,6 +1,6 @@
         </main>
         <footer>
-            <p>Obesity Prevalence Visualizer, Copyright &copy; 2021</p>
+            <p id=copyright>Obesity Prevalence Visualizer, Copyright &copy; 2021</p>
         </footer>
     </body>
 </html>
--- views/components/header.php
+++ views/components/header.php
@@ -1,28 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset='utf-8'>
-        <meta name="viewport" content="width=device-width">
-        <meta name="author" content="Antonio Mihaes, Stoleriu Daniel">
-        <link rel="stylesheet" href="styles.css">
-        <title>Obesity Prevalence Visualizer</title>
-    </head>
-    <body>
-        <header>
-            <div class="container">
-                <div id="branding">
-                    <h1><span class="highlight">Obesity Logo</span></h1>
-                </div>
-                <nav>
-                    <ul>
-                        <li class="current"><a href="#">Home</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">About</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main>
-            <div class="container">
-                <h1>This is the main.</h1>
-            </div>
+  <head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width">
+    <meta name="author" content="Antonio Mihaes, Stoleriu Daniel">
+    <link rel="stylesheet" href="styles.css">
+    <title>Obesity Prevalence Visualizer</title>
+  </head>
+  <body>
+    <header>
+      <h1>Obesity Logo</h1>
+      <nav>
+        <ul>
+          <li class="current"><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>This is the main.</h1>

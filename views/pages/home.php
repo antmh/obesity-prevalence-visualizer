741fb7670c923920ff9abae1fe9c36daf3030487
Antonio Mihăeș <antnm@protonmail.com>
2021-04-03 13:55:09 +0300

    Add presentation cards animations

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,16 +1,22 @@
 <div class="cards-container">
-  <div class="presentation-card">
-    <p>View statistics on Europe provided by Eurostat</p>
-    <div class="presentation-buttons-container">
-      <a class="button" href="https://www.who.int/">Source</a>
-      <a class="button" href="#">View</a>
+  <div class="presentation-card-wrapper">
+    <div class="presentation-card">
+      <h2>Eurostat</h2>
+      <p>View statistics on Europe provided by Eurostat</p>
+      <div class="presentation-buttons-container">
+        <a class="button" href="https://www.who.int/">Source</a>
+        <a class="button" href="#">View</a>
+      </div>
     </div>
   </div>
-  <div class="presentation-card">
-    <p>View statistics on the world provided by the World Health Organization</p>
-    <div class="presentation-buttons-container">
-      <a class="button" href="https://www.who.int/">Source</a>
-      <a class="button" href="#">View</a>
+  <div class="presentation-card-wrapper">
+    <div class="presentation-card">
+      <h2>World Health Organization</h2>
+      <p>View statistics on the world provided by the World Health Organization</p>
+      <div class="presentation-buttons-container">
+        <a class="button" href="https://www.who.int/">Source</a>
+        <a class="button" href="#">View</a>
+      </div>
     </div>
   </div>
 </div>

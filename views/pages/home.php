28d4c7f7716b9a623c0e4caa927e4c5c79df43ee
Antonio Mihăeș <antnm@protonmail.com>
2021-04-03 19:33:42 +0300

    Fix source links

--- views/pages/home.php
+++ views/pages/home.php
@@ -4,7 +4,7 @@
       <h2>Eurostat</h2>
       <p>View statistics on Europe provided by Eurostat</p>
       <div class="presentation-buttons-container">
-        <a class="button" href="https://www.who.int/">Source</a>
+        <a class="button" href="https://ec.europa.eu/eurostat/databrowser/view/sdg_02_10/default/table">Source</a>
         <a class="button" href="#">View</a>
       </div>
     </div>
@@ -14,7 +14,7 @@
       <h2>World Health Organization</h2>
       <p>View statistics on the world provided by the World Health Organization</p>
       <div class="presentation-buttons-container">
-        <a class="button" href="https://www.who.int/">Source</a>
+        <a class="button" href="https://www.who.int/data/gho/data/themes/theme-details/GHO/body-mass-index-(bmi)">Source</a>
         <a class="button" href="#">View</a>
       </div>
     </div>

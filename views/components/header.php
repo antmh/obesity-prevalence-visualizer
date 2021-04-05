04c25e533e9e5010f1d704def4b07f3537f21a3d
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-06 01:07:39 +0300

    Update header.php

--- views/components/header.php
+++ views/components/header.php
@@ -15,6 +15,7 @@
           <li><a class="nav-link" href="/">Home</a></li>
           <li><a class="nav-link" href="/services">Services</a></li>
           <li><a class="nav-link" href="/about">About</a></li>
+          <li><a class="nav-link" href="/login">Login</a></li>
         </ul>
       </nav>
     </header>

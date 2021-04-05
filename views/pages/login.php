58232eb9bf6eabf80bd24ced47fde65739d4b09c
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-06 01:02:57 +0300

    Add login page

---
+++ views/pages/login.php
@@ -0,0 +1,16 @@
+<div class="login-header">
+    <h2>Admin</h2>
+</div>
+<form class="login-form">
+    <div class="input-group">
+        <label>Email</label>
+        <input type="email" name="email" value="" placeholder="Enter email:">
+    </div>
+    <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Enter password:">
+    </div>
+    <div class="input-group">
+        <a class="button">Login</a>
+    </div>
+</form>

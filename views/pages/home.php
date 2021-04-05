7c2a71527175259644e8e1141c30cf4a61d32cff
Antonio Mihăeș <antnm@protonmail.com>
2021-04-05 18:21:40 +0300

    Replace BMI categories image with a description list

--- views/pages/home.php
+++ views/pages/home.php
@@ -20,23 +20,37 @@
     </div>
   </div>
 </div>
-<div>
-    <h1>What is obesity?</h1>
-    <p>
-        Obesity is a medical condition in which excess body fat has accumulated to
-        an extent that it may have a negative effect on health. People are generally considered
-        obese when their body mass index (BMI), a measurement obtained by dividing a person's weight by the
-        square of the person's height—despite known allometric inaccuracies is over 30 kg/m2;
-        the range 25–30 kg/m2 is defined as overweight.
-    </p>
-    <img src="/views/images/BMI_image.jpg" style="width:1000px; height: 600px;">
-    <h1>Facts about overweight and obesity</h1>
-    <p>Some recent WHO global estimates follow.</p>
-    <ul>
-        <li>In 2016, more than 1.9 billion adults aged 18 years and older were overweight. Of these over 650 million adults were obese.</li>
-        <li>In 2016, 39% of adults aged 18 years and over (39% of men and 40% of women) were overweight.</li>
-        <li>Overall, about 13% of the world’s adult population (11% of men and 15% of women) were obese in 2016.</li>
-        <li>The worldwide prevalence of obesity nearly tripled between 1975 and 2016.</li>
-    </ul>
-    <p>Click <a href="#">here</a> to read more.</p>
-</div>
+<h1>What is obesity?</h1>
+<p>
+  Obesity is a medical condition in which excess body fat has accumulated to
+  an extent that it may have a negative effect on health. People are generally considered
+  obese when their body mass index (BMI), a measurement obtained by dividing a person's weight by the
+  square of the person's height—despite known allometric inaccuracies is over 30 kg/m2;
+  the range 25–30 kg/m2 is defined as overweight.
+</p>
+<dl class="bmi-categories-list">
+  <div class="bmi-category">
+    <dt>BMI <18.5</dt>
+    <dd>Underweight</dd>
+  </div>
+  <div class="bmi-category">
+    <dt>BMI 18.5-25</dt>
+    <dd>Normal weight</dd>
+  </div>
+  <div class="bmi-category">
+    <dt>BMI 25-30</dt>
+    <dd>Overweight</dd>
+  </div>
+  <div class="bmi-category">
+    <dt>BMI >30</dt>
+    <dd>Obese</dd>
+  </div>
+</dl>
+<h1>Facts about obesity</h1>
+<p>Some recent WHO global estimates follow.</p>
+<ul>
+  <li>In 2016, more than 1.9 billion adults aged 18 years and older were overweight. Of these over 650 million adults were obese.</li>
+  <li>In 2016, 39% of adults aged 18 years and over (39% of men and 40% of women) were overweight.</li>
+  <li>Overall, about 13% of the world’s adult population (11% of men and 15% of women) were obese in 2016.</li>
+  <li>The worldwide prevalence of obesity nearly tripled between 1975 and 2016.</li>
+</ul>

a161ed5ee0a9d1217bd62011ecf4b9311b8cb3cf
Antonio Mihăeș <antnm@protonmail.com>
2021-04-06 22:00:19 +0300

    Update login form units

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,16 +1,18 @@
-<div class="login-header">
+<div class="login-container">
+  <div class="login-header">
     <h2>Admin</h2>
+  </div>
+  <form class="login-form">
+    <div class="input-group">
+      <label>Email</label>
+      <input type="email" name="email" value="">
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+    <div class="input-group">
+      <button class="button login-button">Login</button>
+    </div>
+  </form>
 </div>
-<form class="login-form">
-    <div class="input-group">
-        <label>Email</label>
-        <input type="email" name="email" value="">
-    </div>
-    <div class="input-group">
-        <label>Password</label>
-        <input type="password" name="password">
-    </div>
-    <div class="input-group">
-        <button class="button login-button">Login</button>
-    </div>
-</form>

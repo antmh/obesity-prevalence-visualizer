08ff3598204c9167571910dfc9c1a6541c8257a5
Antonio Mihăeș <antnm@protonmail.com>
2021-04-06 22:11:59 +0300

    Add for attribute for labels

--- views/pages/login.php
+++ views/pages/login.php
@@ -4,12 +4,12 @@
   </div>
   <form class="login-form">
     <div class="input-group">
-      <label>Email</label>
-      <input type="email" name="email" value="">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email">
     </div>
     <div class="input-group">
-      <label>Password</label>
-      <input type="password" name="password">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password">
     </div>
     <div class="input-group">
       <button class="button login-button">Login</button>

415c0ed8419ffbe48376edf21bf86d9cd91581fc
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-10 13:01:09 +0300

    Create new administration page

---
+++ views/pages/administration.php
@@ -0,0 +1,55 @@
+<div class="administration-cards-container">
+    <div class="administration-card-wrapper">
+        <div class="administration-card">
+            <h1>World Health Organization Data</h1>
+            <button class="button change-data-button">Update</button>
+            <button class="button change-data-button">Clear</button>
+        </div>
+    </div>
+    <div class="administration-card-wrapper">
+        <div class="administration-card">
+            <h1>Eurostat Data</h1>
+            <button class="button change-data-button">Update</button>
+            <button class="button change-data-button">Clear</button>
+        </div>
+    </div>
+    <div class="administration-card-wrapper">
+        <h1>Account</h1>
+        <div class="administration-card">
+            <div class="account-cards-container">
+                <div class="account-card-wrapper">
+                    <div class="account-card">
+                        <h3>Change email</h3>
+                        <div class="input-account">
+                            <label id="account-label" for="password">Password:</label>
+                            <input id="input-account" type="password" name="password" id="password">
+                        </div>
+                        <div class="input-account">
+                            <label id="account-label" for="email">Email:</label>
+                            <input id="input-account" type="email" name="email" id="email">
+                        </div>
+                        <div>
+                            <button class="button change-data-button">Change email</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="account-card-wrapper">
+                    <div class="account-card">
+                        <h3>Change password</h3>
+                        <div class="input-account">
+                            <label id="account-label" for="password">Password:</label>
+                            <input id="input-account" type="password" name="password" id="password">
+                        </div>
+                        <div class="input-account">
+                            <label id="account-label" for="password">New password:</label>
+                            <input id="input-account" type="password" name="password" id="password">
+                        </div>
+                        <div>
+                            <button class="button change-data-button">Change password</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

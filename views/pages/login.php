a3100e51c40b0e8d172fb4f23b2678b86108b82f
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-11 18:53:48 +0300

    Redirect form from login to administration

--- views/pages/login.php
+++ views/pages/login.php
@@ -2,7 +2,7 @@
   <div class="login-header">
     <h2>Admin</h2>
   </div>
-  <form class="login-form" method="post">
+  <form class="login-form" action="administration" method="post">
     <div class="input-login-group">
       <label class="login-label" for="email">Email</label>
       <input class="login-input" type="email" name="email" id="email">
@@ -12,7 +12,8 @@
       <input class="login-input" type="password" name="password" id="password">
     </div>
     <div class="input-login-group">
-      <button class="button login-button" name="LoginButton">Login</button>
+      <input class="button login-button" type="submit" value="Login">
     </div>
   </form>
 </div>
+

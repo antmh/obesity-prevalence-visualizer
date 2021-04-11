b42ee6e5687deb17de2bf3b44d3e7bb0818b5732
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-11 14:28:10 +0300

    Fix classes for inputs and labels

--- views/pages/login.php
+++ views/pages/login.php
@@ -2,17 +2,17 @@
   <div class="login-header">
     <h2>Admin</h2>
   </div>
-  <form class="login-form">
-    <div class="input-login">
-      <label id="login-label" for="email">Email</label>
-      <input id="input-login" type="email" name="email" id="email">
+  <form class="login-form" method="post">
+    <div class="input-login-group">
+      <label class="login-label" for="email">Email</label>
+      <input class="login-input" type="email" name="email" id="email">
     </div>
-    <div class="input-login">
-      <label id="login-label" for="password">Password</label>
-      <input id="input-login" type="password" name="password" id="password">
+    <div class="input-login-group">
+      <label class="login-label" for="password">Password</label>
+      <input class="login-input" type="password" name="password" id="password">
     </div>
-    <div class="input-login">
-      <button class="button login-button">Login</button>
+    <div class="input-login-group">
+      <button class="button login-button" name="LoginButton">Login</button>
     </div>
   </form>
 </div>

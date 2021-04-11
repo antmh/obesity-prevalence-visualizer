dc21052c28a62e1db3e7e4c18362baa452140613
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-04-11 14:28:53 +0300

    Fix classes for inputs and labels

--- views/pages/administration.php
+++ views/pages/administration.php
@@ -2,15 +2,15 @@
     <div class="administration-card-wrapper">
         <div class="administration-card">
             <h1>World Health Organization Data</h1>
-            <button class="button change-data-button">Update</button>
-            <button class="button change-data-button">Clear</button>
+            <button class="button administration-button">Update</button>
+            <button class="button administration-button">Clear</button>
         </div>
     </div>
     <div class="administration-card-wrapper">
         <div class="administration-card">
             <h1>Eurostat Data</h1>
-            <button class="button change-data-button">Update</button>
-            <button class="button change-data-button">Clear</button>
+            <button class="button administration-button">Update</button>
+            <button class="button administration-button">Clear</button>
         </div>
     </div>
     <div class="administration-card-wrapper">
@@ -20,32 +20,32 @@
                 <div class="account-card-wrapper">
                     <div class="account-card">
                         <h3>Change email</h3>
-                        <div class="input-account">
-                            <label id="account-label" for="password">Password:</label>
-                            <input id="input-account" type="password" name="password" id="password">
+                        <div class="input-admin-group">
+                            <label class="account-label" for="password">Password:</label>
+                            <input class="account-input" type="password" name="password" id="password">
                         </div>
-                        <div class="input-account">
-                            <label id="account-label" for="email">Email:</label>
-                            <input id="input-account" type="email" name="email" id="email">
+                        <div class="input-admin-group">
+                            <label class="account-label" for="email">Email:</label>
+                            <input class="account-input" type="email" name="email" id="email">
                         </div>
                         <div>
-                            <button class="button change-data-button">Change email</button>
+                            <button class="button administration-button">Change email</button>
                         </div>
                     </div>
                 </div>
                 <div class="account-card-wrapper">
                     <div class="account-card">
                         <h3>Change password</h3>
-                        <div class="input-account">
-                            <label id="account-label" for="password">Password:</label>
-                            <input id="input-account" type="password" name="password" id="password">
+                        <div class="input-admin-group">
+                            <label class="account-label" for="oldpassword">Password:</label>
+                            <input class="account-input" type="password" name="oldpassword" id="oldpassword">
                         </div>
-                        <div class="input-account">
-                            <label id="account-label" for="password">New password:</label>
-                            <input id="input-account" type="password" name="password" id="password">
+                        <div class="input-admin-group">
+                            <label class="account-label" for="newpassword">New password:</label>
+                            <input class="account-input" type="password" name="newpassword" id="newpassword">
                         </div>
                         <div>
-                            <button class="button change-data-button">Change password</button>
+                            <button class="button administration-button">Change password</button>
                         </div>
                     </div>
                 </div>

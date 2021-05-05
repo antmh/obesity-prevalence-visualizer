abe608e87c8d4c66c60724a2a39ab1836eb0919f
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-05-05 16:32:50 +0300

    Update Authentication

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -10,6 +10,9 @@ class LoginController extends Controller
 {
     public function index(): void
     {
+        if (isset($_POST['verifyLogin'])) {
+            \models\Login::verifyLogin($_POST['email'], $_POST['password']);
+        }
         \views\View::render('login.php');
     }
 }

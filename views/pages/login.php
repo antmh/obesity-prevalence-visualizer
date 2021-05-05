6f42633ab9fcbdaab36da3c97f2f42fee7ff8e20
stoleriudaniel <75793131+stoleriudaniel@users.noreply.github.com>
2021-05-05 16:42:13 +0300

    Add error for login

--- views/pages/login.php
+++ views/pages/login.php
@@ -2,7 +2,7 @@
   <div class="login-header">
     <h2>Admin</h2>
   </div>
-  <form class="login-form" action="administration" method="post">
+  <form class="login-form" method="post"> <!--action="administration" -->
     <div class="input-login-group">
       <label class="login-label" for="email">Email</label>
       <input class="login-input" type="email" name="email" id="email">
@@ -11,8 +11,15 @@
       <label class="login-label" for="password">Password</label>
       <input class="login-input" type="password" name="password" id="password">
     </div>
+      <?php
+
+      if (isset($_SESSION['ERR'])) {
+          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['ERR'] . '</div>';
+          unset($_SESSION['ERR']);
+      }
+        ?>
     <div class="input-login-group">
-      <input class="button login-button" type="submit" value="Login">
+      <input class="button login-button" type="submit" name="verifyLogin" value="Login">
     </div>
   </form>
 </div>

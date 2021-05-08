d89854538a8c2c34f0c622d19050ba823fed2487
Antonio Mihăeș <antnm@protonmail.com>
2021-05-08 15:11:40 +0300

    Use fieldsets

--- views/pages/eurostat.php
+++ views/pages/eurostat.php
@@ -1,12 +1,20 @@
-<form>
-  <h2><?= $radioGroup['name'] ?></h2>
-  <?php include('views/components/radioGroup.php'); ?>
-  <h2><?= $checkboxGroup['name'] ?></h2>
-  <?php include('views/components/checkboxGroup.php'); ?>
-  <h2><?= $orderGroup['name'] ?></h2>
-  <?php include('views/components/orderGroup.php'); ?>
-  <h2>Filter</h2>
-  <?php include('views/components/accordion.php'); ?>
+<form class="select-form">
+  <fieldset>
+    <legend><?= $radioGroup['name'] ?></legend>
+    <?php include('views/components/radioGroup.php'); ?>
+  </fieldset>
+  <fieldset>
+    <legend><?= $checkboxGroup['name'] ?></legend>
+    <?php include('views/components/checkboxGroup.php'); ?>
+  </fieldset>
+  <fieldset>
+    <legend><?= $orderGroup['name'] ?></legend>
+    <?php include('views/components/orderGroup.php'); ?>
+  </fieldset>
+  <fieldset>
+    <legend>Filter</legend>
+    <?php include('views/components/accordion.php'); ?>
+  </fieldset>
   <input class="button" type="submit" value="Submit">
 </form>
 <?php include('views/components/barChart.php'); ?>
